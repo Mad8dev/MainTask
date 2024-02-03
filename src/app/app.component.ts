0f242054af508802cfb3a54ad032a48ae067e672
Mad*Dev <superdevmadrimov@gmail.com>
2024-02-03 21:58:14 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgForOf } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NgForOf, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'newTable';
+  data: any;
+  currentPage: number = 1;
+  itemsPerPage: number = 10;
+  totalItems: number = 0;
+
+  constructor(private http: HttpClient) {}
+  ngOnInit() {
+    this.getAllProduct();
+  }
+
+  getAllProduct() {
+    const url = 'https://kep.uz/api/problems';
+
+    const params = {
+      page: this.currentPage.toString(),
+      limit: this.itemsPerPage.toString(),
+    };
+    this.http.get(url, { params }).subscribe((response: any) => {
+      this.data = response;
+      this.totalItems = response.total;
+    });
+  }
+
+  onPageChange(page: number) {
+    this.currentPage = page;
+    this.getAllProduct();
+    console.log(page);
+  }
 }
